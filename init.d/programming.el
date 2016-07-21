de2387c3696fb57de1880c14d12cd4315ba4cacf
benn <sjembn@gmail.com>
2016-07-21 09:29:52 +0800

    change js2-mode

--- init.d/programming.el
+++ init.d/programming.el
@@ -117,44 +117,47 @@
 
 ;; ------ Javascript
 
-(use-package js2-mode
-  :disabled t  
-  :mode (("\\.js\\'" . js2-mode))
-  :config
-  )
-
-(use-package js2-refactor
-  :disabled t  
-  :config
-  (add-hook 'js2-mode-hook #'js2-refactor-mode)
-  (js2r-add-keybindings-with-prefix "C-c C-m")
-  ;; (js2r-add-keybindings-with-modifier "C-s-")
-  )
-
-(use-package simple-httpd)
-(use-package skewer-mode
-  :disabled t
-  :config
-  (skewer-setup)
-  (add-hook 'web-mode-hook 'skewer-mode))
-
-(use-package js-comint
-  :disabled t
-  :config
-  (add-hook 'js2-mode-hook '(lambda ()
-                              (local-set-key "\C-x\C-e" 'js-send-last-sexp)
-                              (local-set-key "\C-\M-x" 'js-send-last-sexp-and-go)
-                              (local-set-key "\C-cb" 'js-send-buffer)
-                              (local-set-key "\C-c\C-b" 'js-send-buffer-and-go)
-                              (local-set-key "\C-cl" 'js-load-file-and-go))))
+;; (use-package js-comint
+;;   :disabled t
+;;   :config
+;;   (add-hook 'js2-mode-hook '(lambda ()
+;;                               (local-set-key "\C-x\C-e" 'js-send-last-sexp)
+;;                               (local-set-key "\C-\M-x" 'js-send-last-sexp-and-go)
+;;                               (local-set-key "\C-cb" 'js-send-buffer)
+;;                               (local-set-key "\C-c\C-b" 'js-send-buffer-and-go)
+;;                               (local-set-key "\C-cl" 'js-load-file-and-go))))
 
 (use-package js3-mode
+  :disabled t
   :mode (("\\.js\\'" . js3-mode)
          ("\\.json\\'" . js3-mode)))
 
+
+(use-package js2-mode
+  :mode (("\\.js\\'" . js2-mode)
+         ("\\.json\\'" . js3-mode))
+  :bind (:map js2-mode-map
+         ("<return>" . newline-and-indent))
+  :config
+  (use-package js2-refactor
+    :config
+    (add-hook 'js2-mode-hook #'js2-refactor-mode)
+    (js2r-add-keybindings-with-prefix "C-c C-m")
+    ;; (js2r-add-keybindings-with-modifier "C-s-")
+    )
+
+  (use-package skewer-mode
+    :init
+    (use-package simple-httpd)
+    :config
+    (skewer-setup)
+    (add-hook 'js2-mode-hook 'skewer-mode)
+    (add-hook 'web-mode-hook 'skewer-mode)))
+
+
 (use-package tern
   :config
-  (add-hook 'js3-mode-hook (lambda () (tern-mode t)))
+  (add-hook 'js2-mode-hook (lambda () (tern-mode t))))
 
   (use-package company-tern
     :config
@@ -165,13 +168,16 @@
     ))
 
 (use-package nodejs-repl
+  :disabled t
   :config
-  (add-hook 'js3-mode-hook '(lambda ()
-                              (local-set-key "\C-x\C-e" 'nodejs-repl-send-last-sexp)
-                              (local-set-key "\C-\M-x" 'nodejs-repl-send-last-sexp-and-go)
-                              (local-set-key "\C-cb" 'nodejs-repl-send-buffer)
-                              (local-set-key "\C-c\C-b" 'nodejs-repl-send-buffer-and-go)
-                              (local-set-key "\C-cl" 'nodejs-repl-load-file-and-go))))
+  (defun my-nodejs-repl-hook()
+    (local-set-key "\C-x\C-e" 'nodejs-repl-send-last-sexp)
+    (local-set-key "\C-\M-x" 'nodejs-repl-send-last-sexp-and-go)
+    (local-set-key "\C-cb" 'nodejs-repl-send-buffer)
+    (local-set-key "\C-c\C-b" 'nodejs-repl-send-buffer-and-go)
+    (local-set-key "\C-cl" 'nodejs-repl-load-file-and-go))
+
+  (add-hook 'js2-mode-hook 'my-nodejs-repl-hook))
 
 ;; ------ CEDET
 
