b5089ba86baf0fda65ba114f296b95e32f5fa653
benn <sjembn@gmail.com>
2016-08-02 07:08:38 +0800

    add markdown-mode

--- init.d/programming.el
+++ init.d/programming.el
@@ -250,3 +250,10 @@
 (use-package dtrt-indent
   :config
   (dtrt-indent-mode 1))
+
+(use-package markdown-mode
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
