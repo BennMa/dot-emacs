37aab2d32956968c995bcd68ec52ee0a0bf9c4a1
benn <benn@thenetcircle.com>
2017-02-21 18:33:49 +0800

    some improvements

--- features/base.el
+++ features/base.el
@@ -1,9 +1,80 @@
+;; ------ global keybindings
+(use-package general
+  :config
+  (progn 
+    (use-package key-chord :defer 1 :config (setq key-chord-two-keys-delay 0.2))
+    ;; C-
+    (general-define-key "C-a" 'blaine/beginning-of-line
+                        "C-M-j" '(lambda() (interactive) (delete-indentation t))
+                        "C-c C-r" 'ivy-resume)
+    ;; M-
+    (general-define-key "M-!" 'async-shell-command
+                        "<M-backspace>" 'blaine/contextual-backspace
+                        "M-`" 'other-frame
+                        "M-p" 'hydra-projectile/body)
+    ;; C-x-
+    (general-define-key :prefix "C-x"
+                        "d" 'delete-whitespace-rectangle
+                        "F" 'set-fill-column
+                        "t" 'toggle-truncate-lines
+                        "-" 'split-window-below
+                        "|" 'split-window-right
+                        "y" '(lambda() (interactive) (blaine/insert-separator nil))
+                        "Y" '(lambda() (interactive) (blaine/insert-separator t))
+                        "v" 'blaine/buffer-info
+                        ;; ------ C-x C-
+                        "C-d" 'blaine/duplicate-line
+                        "C-j" 'dired-jump
+                        "C-e" 'pp-eval-last-sexp
+                        "C-o" 'blaine/kill-other-buffers)
+    ;; C-c-
+    (general-define-key :prefix "C-c"
+                        "SPC" 'just-one-space
+                        "C-f" 'blaine/format-buffer
+                        "f"   'flush-lines
+                        "g"   'goto-line
+                        "k"   'keep-lines
+                        "o"   'customize-option
+                        "O"   'customize-group
+                        "F"   'customize-face
+                        "q"   'fill-region
+                        "["   'align-regexp
+                        "="   'count-matches
+                        ";"   'comment-or-uncomment-region)
+    ;; C-c e -
+    (general-define-key :prefix "C-c e"
+                        "E" 'elint-current-buffer
+                        "b" '(lambda () (interactive) (call-interactively 'eval-buffer))
+                        "C" 'cancel-debug-on-entry
+                        "d" 'debug-on-entry
+                        "e" 'toggle-debug-on-error
+                        "f" 'emacs-lisp-byte-compile-and-load
+                        "j" 'emacs-lisp-mode
+                        "l" 'find-library
+                        "r" '(lambda () (interactive) (call-interactively 'eval-region))
+                        "z" 'byte-recompile-directory)
+    ;; C-h e -
+    (define-prefix-command 'blaine--lisp-find-map)
+    (bind-key "C-h e" 'blaine--lisp-find-map)
+    (general-define-key :keymaps 'blaine--lisp-find-map
+                        "c" 'finder-commentary
+                        "e" 'view-echo-area-messages
+                        "f" 'find-function
+                        "F" 'find-face-definition
+                        "i" 'info-apropos
+                        "k" 'find-function-on-key
+                        "l" 'find-library
+                        "s" 'blaine/scratch
+                        "v" 'find-variable
+                        "V" 'apropos-value
+                        "t" 'blaine/what-face)
+    ))
+
+;; ------ packages
 (let ((use-package-always-ensure t))
 
   (use-package diminish)
   (use-package bind-key)
-  (use-package general)
-  (use-package key-chord :defer 1 :config (setq key-chord-two-keys-delay 0.2))
   (use-package use-package-chords :config (key-chord-mode 1))
   (use-package server :config (unless (server-running-p) (server-start)))
   (use-package restart-emacs)
@@ -47,20 +118,23 @@
 
   (use-package projectile
     :diminish (projectile-mode . " ⓟ")
-    :commands (projectile-global-mode
-               projectile-mode
+    :commands (projectile-mode
+               projectile-global-mode
                hydra-projectile/body
                hydra-projectile-if-projectile-p
                projectile-project-root
                projectile-project-p
                counsel-projectile-switch-to-buffer)
-    :bind (("M-p" . hydra-projectile/body))
     :config
     (progn 
       (projectile-global-mode 1)
-
       ;; https://github.com/ericdanan/counsel-projectile
       (use-package counsel-projectile :config (counsel-projectile-on))
+      (defun hydra-projectile-if-projectile-p ()
+        (interactive)
+        (if (projectile-project-p)
+            (hydra-projectile/body)
+          (counsel-projectile)))
 
       (defhydra hydra-projectile (:color teal :hint nil)
         "
@@ -91,13 +165,7 @@
         ("x"   projectile-remove-known-project)
         ("X"   projectile-cleanup-known-projects)
         ("z"   projectile-cache-current-file)
-        ("q"   nil "cancel" :color blue))
-
-      (defun hydra-projectile-if-projectile-p ()
-        (interactive)
-        (if (projectile-project-p)
-            (hydra-projectile/body)
-          (counsel-projectile)))))
+        ("q"   nil "cancel" :color blue))))
 
   (use-package exec-path-from-shell
     :defer 2
@@ -107,88 +175,3 @@
     (exec-path-from-shell-initialize))
 
   ) ;; let ends here
-
-
-;; ------ global keybindings
-;; C-
-(general-define-key "C-a" 'blaine/beginning-of-line
-                    "C-M-j" '(lambda() (interactive) (delete-indentation t))
-                    "C-. m" 'kmacro-keymap
-                    "C-. C-i" 'indent-rigidly
-                    "C-c C-r" 'ivy-resume)
-
-;; M-
-(general-define-key "M-!" 'async-shell-command
-                    "M-'" 'insert-pair
-                    "<M-backspace>" 'blaine/contextual-backspace
-                    "M-`" 'other-frame
-                    "M-W" 'mark-word
-                    "M-X" 'mark-sexp
-                    "M-D" 'mark-defun
-                    "M-g c" 'goto-char
-                    "M-g l" 'goto-line)
-
-;; C-x-
-(general-define-key :prefix "C-x"
-                    "d" 'delete-whitespace-rectangle
-                    "F" 'set-fill-column
-                    "t" 'toggle-truncate-lines
-                    "_" 'split-window-below
-                    "|" 'split-window-right
-                    "y" '(lambda() (interactive) (blaine/insert-separator nil))
-                    "Y" '(lambda() (interactive) (blaine/insert-separator t))
-                    "v" 'blaine/buffer-info
-                    ;; ------ C-x C-
-                    "C-d" 'blaine/duplicate-line
-                    "C-j" 'dired-jump
-                    "C-e" 'pp-eval-last-sexp
-                    "C-n" 'next-line
-                    "C-o" 'blaine/kill-other-buffers
-                    "M-n" 'set-goal-column)
-
-;; C-c-
-(general-define-key :prefix "C-c"
-                    "SPC" 'just-one-space
-                    "C-f" 'blaine/format-buffer
-                    "f" 'flush-lines
-                    "g" 'goto-line
-                    "k" 'keep-lines
-                    "o" 'customize-option
-                    "O" 'customize-group
-                    "F" 'customize-face
-                    "q" 'fill-region
-                    "r" 'replace-regexp
-                    "s" 'replace-string
-                    "u" 'rename-uniquely
-                    "v" 'ffap
-                    "z" 'clean-buffer-list
-                    "[" 'align-regexp
-                    "=" 'count-matches
-                    ";" 'comment-or-uncomment-region)
-
-(general-define-key :prefix "C-c e"
-                    "E" 'elint-current-buffer
-                    "b" '(lambda () (interactive) (call-interactively 'eval-buffer))
-                    "C" 'cancel-debug-on-entry
-                    "d" 'debug-on-entry
-                    "e" 'toggle-debug-on-error
-                    "f" 'emacs-lisp-byte-compile-and-load
-                    "j" 'emacs-lisp-mode
-                    "l" 'find-library
-                    "r" '(lambda () (interactive) (call-interactively 'eval-region))
-                    "z" 'byte-recompile-directory)
-;; C-h e -
-(define-prefix-command 'blaine--lisp-find-map)
-(bind-key "C-h e" 'blaine--lisp-find-map)
-(general-define-key :keymaps 'blaine--lisp-find-map
-                    "c" 'finder-commentary
-                    "e" 'view-echo-area-messages
-                    "f" 'find-function
-                    "F" 'find-face-definition
-                    "i" 'info-apropos
-                    "k" 'find-function-on-key
-                    "l" 'find-library
-                    "s" 'blaine/scratch
-                    "v" 'find-variable
-                    "V" 'apropos-value
-                    "t" 'blaine/what-face)
